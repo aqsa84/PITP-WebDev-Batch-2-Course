c1eef8977f2176c22ad85e2eb0e7ea85029d248b
Aqsa <syedamiss004@gmail.com>
2025-12-18 17:14:40 +0500

    first commit

---
+++ example.js
@@ -0,0 +1,6 @@
+const product = "Laptop";
+const price =999.99; // template Literal Examples
+const summary = `The ${product} is priced at 
+${price.toFixed(2)}. It is a great deal!`;
+  console.log(summary)
+    
---
+++ week3/script.js
@@ -0,0 +1,10 @@
+//var,let,const
+//var name ="g1"
+//var name ="g2"
+//var name ="g3"
+//alert (name);
+
+
+let Message="hi";
+
+
